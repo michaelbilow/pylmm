c160b3b514df15a09c6d343768f1d9a423453b30
Kevin Middleton <middletonk@missouri.edu>
2015-12-13 21:37:50 -0600

    Fix mixed tab and space indentation

--- pylmm/misc.py
+++ pylmm/misc.py
@@ -22,41 +22,41 @@ import numpy as np
 from pylmm.lmm import LMM
 
 def fitTwo(y,K1,K2,X0=None,wgrids=100):
-      '''
-	 Simple function to fit a model with two variance components.
-	 It works by running the standard pylmm algorithm in a loop
-	 where at each iteration of the loop a new kinship is generated
-	 as a linear combination of the original two.
-      '''
+	'''
+		Simple function to fit a model with two variance components.
+		It works by running the standard pylmm algorithm in a loop
+		where at each iteration of the loop a new kinship is generated
+		as a linear combination of the original two.
+	'''
 
-      # Create a uniform grid
-      W = np.array(range(wgrids)) / float(wgrids)
-      Res = []
-      LLs = []
+	# Create a uniform grid
+	W = np.array(range(wgrids)) / float(wgrids)
+	Res = []
+	LLs = []
 
-      for w in W:
-	 # heritability will be estimated for linear combo of kinships
-	 K = w*K1 + (1.0 - w)*K2
-	 sys.stderr.write("Fitting weight %0.2f\n" % (w))
-	 L = LMM(y,K,X0=X0)
-	 R = L.fit()
-	 Res.append(R)
-	 LLs.append(R[-1])
-      
-	 del K
+	for w in W:
+		# heritability will be estimated for linear combo of kinships
+		K = w*K1 + (1.0 - w)*K2
+		sys.stderr.write("Fitting weight %0.2f\n" % (w))
+		L = LMM(y,K,X0=X0)
+		R = L.fit()
+		Res.append(R)
+		LLs.append(R[-1])
+		
+		del K
 
-      L = np.array(LLs)
-      i = np.where(L == L.max())[0]
-      if len(i) > 1:
-	 sys.stderr.write("WARNING: Found multiple maxes using first one\n")
+	L = np.array(LLs)
+	i = np.where(L == L.max())[0]
+	if len(i) > 1:
+		sys.stderr.write("WARNING: Found multiple maxes using first one\n")
 
-      i = i[0]
-      hmax,beta,sigma,LL = Res[i]
-      w = W[i]
+	i = i[0]
+	hmax,beta,sigma,LL = Res[i]
+	w = W[i]
 
-      h1 = w * hmax 
-      h2 = (1.0 - w) * hmax 
-      e = (1.0 - hmax) 
+	h1 = w * hmax 
+	h2 = (1.0 - w) * hmax 
+	e = (1.0 - hmax) 
 
-      return h1,h2,e,beta,sigma,LL
- 
+	return h1,h2,e,beta,sigma,LL
+
