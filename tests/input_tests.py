57a24e7f61c0b48eb3e1ae007b9fd12873b6e352
michaelbilow <bilow@cs.ucla.edu>
2016-06-28 09:52:46 -0700

    allowed input tests to import plink class

--- tests/input_tests.py
+++ tests/input_tests.py
@@ -1,7 +1,9 @@
-import input
+from pylmm.input import plink
+
 
 def test1():
     return True
+
 
 def run_all_tests():
     output1 = test1()
@@ -9,3 +11,5 @@ def run_all_tests():
 
 if __name__ == "__main__":
     run_all_tests()
+
+    my_plink_object = plink() ## FIX ME
