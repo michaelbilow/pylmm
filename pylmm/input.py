f82a5a44dbba09cfd312899238e7a2a6abaedd46
michaelbilow <bilow@cs.ucla.edu>
2016-07-08 09:27:47 -0700

    updated spacing

--- pylmm/input.py
+++ pylmm/input.py
@@ -109,7 +109,7 @@ class plink:
         self.have_read = 0
         self.snpFileHandle = open(file, 'r')
 
-        self.BytestoRead = self.N / 4 + (self.N % 4 and 1 or 0)
+        self.BytestoRead = int(float(self.N) / 4 + (self.N % 4 and 1 or 0)
         self._formatStr = 'c' * self.BytestoRead
 
         file = self.fbase + '.bed'
